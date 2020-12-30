dec158f045ef6ba14908a45deb20c1673a26cb7e
StefanosRigas <stefanosrigas97@gmail.com>
2020-12-30 14:11:11 +0200

    Revert "Added function that saves the matrix"
    This reverts commit e32d4fabcf9ba183bf1385fd791f96b338ca8f2f.

--- main.py
+++ main.py
@@ -9,9 +9,8 @@ from sklearn.neighbors import NearestNeighbors
 import os
 os.chdir(r'C:\Users\Stefanos\OneDrive\Υπολογιστής\Fast Projects\Decision Theory\Decision-Theory')
 
-# ---------------------------------------------------------------------------------------------------------------------------------------------------
-
 # read data with panda, only the columns that are needed
+
 r_cols = ['user_id', 'movie_id', 'rating']
 ratings = pd.read_csv('ratings.csv', sep=';', names=r_cols, usecols=[1, 2, 3], encoding="ISO-8859-1", low_memory=False, header=0)
 
@@ -20,25 +19,27 @@ movies = pd.read_csv('movies.csv', sep='\t', names=m_cols, usecols=[1, 2], encod
 
 # create m*n matrix where m is the number of the users and n the number of the movies,
 # each cell contains the rating of user i for the movie j
+
 matrix = ratings.pivot(index='user_id', columns='movie_id', values='rating').fillna(0)
 
+
 # convert dataframe of movie features to scipy sparse matrix for efficiency
+
 features = csr_matrix(matrix.values)
 
-# save matrix to a csv file in case we want to manually check the data
-# matrix.to_csv("matrix.csv", header=None, index=None)
-
-# ---------------------------------------------------------------------------------------------------------------------------------------------------
 
 # train knn model, using cosine as similarity metric
+
 knnModel = NearestNeighbors(metric="cosine", algorithm="brute", n_neighbors=10)
 knnModel.fit(features)
 
 # get the 10 nearest neighbors of user
+
 distances, indexes = knnModel.kneighbors([matrix.loc[1, :]])
 
 
 # recommend 5 movies to the user that he has not already seen
+
 users = matrix.reindex(indexes[0] + 1)
 moviesRec = np.sum(users, 0)
 
@@ -47,9 +48,8 @@ for i in range(1, len(moviesRec)):
         if matrix.loc[1, i] != 0:
             moviesRec[i] = 0
     except KeyError:
-        # do nothing
         pass
-    
-# reminder to fix the output format
+
+# note to fix the output format
 
 print('You should also watch:\n', movies[['title']].loc[movies['movie_id'] == moviesRec.idxmax()])
