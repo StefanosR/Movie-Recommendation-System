8d3960b2e7301d64800a2b43a0f51fd0422055e2
Asteryx21 <63299172+Asteryx21@users.noreply.github.com>
2021-02-22 18:04:17 +0200

    Update test.py

--- test.py
+++ test.py
@@ -25,9 +25,10 @@ movie_ratings = pd.merge(movies_info, ratings)
 merged = pd.merge(movie_ratings, users_info)
 
 ## timestamp to date
-merged['timestamp'] = [time.strftime(' %d-%m-%Y', time.localtime(x)) for x in merged['timestamp']]
+merged['timestamp'] = [time.strftime('%Y', time.localtime(x)) for x in merged['timestamp']]
+# %d-%m-
 
-#average rating diference between genders
+##average rating diference between genders
 pivoted = merged.pivot_table(index=['movie_id', 'title'],
                            columns=['gender'],
                            values='rating',
@@ -41,17 +42,49 @@ disagreements.sort_values().plot(kind='barh', figsize=[9, 15])
 plt.title('Male vs. Female Avg. Ratings\n(Difference > 0 = Favored by Men)')
 plt.ylabel('Title')
 plt.xlabel('Average Rating Difference');
+
+
 plt.show()
-
-
 ##find popular movie depending on age desc
-age_dataframe = merged[['movie_id','title','user_id','rating','age_desc']]
-#by_age =age_dataframe.groupby(['movie_id','title','age_desc']).size()
-by_age = age_dataframe.groupby(['title','age_desc'])['rating'].mean().unstack(1).fillna(0)
-#by_age['average'] = age_dataframe.groupby(['movie_id','title','age_desc'])['rating'].mean() 
-print(by_age)
+
+by_age = merged.pivot_table(index=['movie_id', 'title'],
+                           columns=['age_desc'],
+                           values='rating',
+                           fill_value=0)
+
+age_df = (merged.groupby(['age_desc','title'])['rating'].size().sort_values(ascending=False) 
+   .reset_index(name='counts') 
+   .drop_duplicates(subset='age_desc'))
+print(age_df)
+
+maxValues_age = by_age.max() 
+maxValueIndex_age = by_age.idxmax()
+#print( maxValueIndex_age)
 
 ##find popular movie depending on occupation desc
-occ_dataframe = merged[['movie_id','title','user_id','rating','occ_desc']]
-by_occ = occ_dataframe.groupby(['title','occ_desc'])['rating'].mean().unstack(1).fillna(0)
-print(by_occ)
+
+by_occ = merged.pivot_table(index=['movie_id', 'title'],
+                           columns=['occ_desc'],
+                           values='rating',
+                           fill_value=0)
+occ_df = (merged.groupby(['occ_desc','title'])['rating'].size().sort_values(ascending=False) 
+   .reset_index(name='counts') 
+   .drop_duplicates(subset='occ_desc'))
+print(occ_df)
+
+maxValues_occ = by_occ.max() 
+maxValueIndex_occ = by_occ.idxmax()
+#print( maxValueIndex_occ)
+
+##find popular movie depending on zipcode
+zip_df = (merged.groupby(['zipcode','title'])['rating'].size().sort_values(ascending=False) 
+   .reset_index(name='counts') 
+   .drop_duplicates(subset='zipcode'))
+print(zip_df)
+
+##find popular movie depending on year
+
+year_df = (merged.groupby(['timestamp','title'])['rating'].size().sort_values(ascending=False) 
+   .reset_index(name='counts') 
+   .drop_duplicates(subset='timestamp'))
+print(year_df)
